1f33c1ac2dade522fd1c58182d3c0004fe687202
Mai <mai.uchida.columbia@gmail.com>
2020-06-16 00:08:34 -0400

    update

--- libs/gitbook-2.6.7/js/plugin-sharing.js
+++ libs/gitbook-2.6.7/js/plugin-sharing.js
@@ -1,63 +1,13 @@
 gitbook.require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
     var SITES = {
-        'github': {
-            'label': 'Github',
-            'icon': 'fa fa-github',
-            'onClick': function(e) {
-                e.preventDefault();
-                var repo = $('meta[name="github-repo"]').attr('content');
-                if (typeof repo === 'undefined') throw("Github repo not defined");
-                window.open("https://github.com/"+repo);
-            }
-        },
-        'facebook': {
-            'label': 'Facebook',
-            'icon': 'fa fa-facebook',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open("http://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(location.href));
-            }
-        },
         'twitter': {
             'label': 'Twitter',
             'icon': 'fa fa-twitter',
             'onClick': function(e) {
                 e.preventDefault();
-                window.open("http://twitter.com/intent/tweet?text="+document.title+"&url="+encodeURIComponent(location.href)+"&hashtags=rmarkdown,bookdown");
+                window.open("https://twitter.com/CUIMClibrary");
             }
         },
-        'linkedin': {
-            'label': 'LinkedIn',
-            'icon': 'fa fa-linkedin',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open("https://www.linkedin.com/shareArticle?mini=true&url="+encodeURIComponent(location.href)+"&title="+encodeURIComponent(document.title));
-            }
-        },
-        'weibo': {
-            'label': 'Weibo',
-            'icon': 'fa fa-weibo',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open("http://service.weibo.com/share/share.php?content=utf-8&url="+encodeURIComponent(location.href)+"&title="+encodeURIComponent(document.title));
-            }
-        },
-        'instapaper': {
-            'label': 'Instapaper',
-            'icon': 'fa fa-italic',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open("http://www.instapaper.com/text?u="+encodeURIComponent(location.href));
-            }
-        },
-        'vk': {
-            'label': 'VK',
-            'icon': 'fa fa-vk',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open("http://vkontakte.ru/share.php?url="+encodeURIComponent(location.href));
-            }
-        }
     };
 
 
